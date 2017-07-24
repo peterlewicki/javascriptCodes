9944dd2dac0783a0ef97ffe371339ef4c057218c
Peter Lewicki <bryonylewicki@Bryonys-iMac.local>
2017-07-24 08:47:40 -0400

    Various javascript codes I’ve hammered out while learning the language.

---
+++ golfScore.js
@@ -0,0 +1,34 @@
+/* golf score.  part of the Free Code Camp curriculum, learning to chain together else if statements */
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  }
+  else if (strokes <= par -2) {
+    return "Eagle";
+  }
+  else if (strokes == par -1) {
+    return "Birdie";
+  }
+   else if (strokes === par) {
+    return "Par";
+  }
+else if (strokes === par+1) {
+  return "Bogey";
+}
+  
+  else if (strokes == par+2) {
+    return "Double Bogey";
+    
+  }
+  
+  else if (strokes >= par+3) {
+    return "Go Home!";
+  }
+  
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(4, 4);
